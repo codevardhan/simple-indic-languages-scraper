8a062580db1c2c15a51e3e977d3c1110f593571a
Harshavardhan M <harsha7vardhan@gmail.com>
2022-09-12 21:14:03 +0530

    added cli funtionality

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# simple-indic-languages-scraper
+# Indic news data scraper
+
+![Status](https://img.shields.io/badge/status-completed-yellow)<br>
+![Issues](https://img.shields.io/badge/issues-0-blue)<br>
+[![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)](http://commonmark.org)<br>
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)<br>
+
 A simple scraper to scrape news archives from https://www.oneindia.com in a few low resource languages for use in NLP research/projects.
+
+# How it works
+Takes in the language and save directory as arguments (defaults to current directory)
+<ul>
+  <li>malayalam</li>
+  <li>telugu</li>
+  <li>kannada</li>
+  <li>tamil</li>
+  <li>odia</li>
+  <li>bengali</li>
+  <li>gujarati</li>
+  <li>hindi</li>
+</ul> 
+
+## Technology Stack 💻
+- Python
+- BeautifulSoup
+
+
+## Future Scope
+
+Add custom date selector argument to get the data of a specific date period. Currently defaults to Jan 1st 2022 - 2000 May 5th.
--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import time
+import os
 from tqdm import tqdm, trange
 from datetime import date, timedelta
+from argparse import ArgumentParser
+
+CODES = {
+    "telugu": "te",
+    "malayalam": "ml",
+    "bengali": "bn",
+    "gujarati": "gj",
+    "hindi": "hn",
+    "tamil": "tn",
+    "odia": "od",
+    "kannada": "kn",
+}
 
 
 def daterange(start_date, end_date):
@@ -11,13 +24,15 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 
-def get_data(url, lang):
+def get_data(url, lang, save_dir):
+    url = url.replace("language", args.lang)
     end_date = date(2022, 4, 1)
     start_date = date(2000, 5, 1)
     data = []
     for single_date in daterange(start_date, end_date):
         url_end = single_date.strftime("%Y-%m-%d").replace("-", "/") + "/"
         page_url = url + url_end
+        print(page_url)
         main_soup = BeautifulSoup(requests.get(page_url).content, features="html5lib")
         check = main_soup.find_all("section")
         if "Page not found" not in check:
@@ -31,8 +46,47 @@ def get_data(url, lang):
                     for elem in news_soup.findAll("p"):
                         doc_data += str(elem).replace("\n", "")
                     data.append(doc_data)
-            with open("monolingual." + lang, "w") as f:
+            with open(os.path.join(save_dir, "monolingual.") + CODES[lang], "w") as f:
                 f.writelines(data)
 
+
 if __name__ == "__main__":
-    get_data("https://telugu.oneindia.com/", "te")
+    url = "https://language.oneindia.com/"
+    parser = ArgumentParser(
+        description="Creating a scraper for getting news data from oneindia.com, in the preferred language.",
+        epilog="Use the data well! :)",
+    )
+
+    def dir_path(string):
+        if os.path.isdir(string):
+            return string
+        else:
+            raise NotADirectoryError(string)
+
+    def def_lang(string):
+        if string in CODES.keys():
+            return string
+        else:
+            raise Exception(
+                "LanguageError: Choose one of the supported languages. Check help for details."
+            )
+
+    parser.add_argument(
+        "lang",
+        metavar="lang",
+        type=def_lang,
+        help="The language in which the news article should be scraped. Supported languages - hindi, malayalam, telugu, tamil, kannada, odia, bengali, gujarati",
+    )
+
+    parser.add_argument(
+        "--save_dir",
+        metavar="save_dir",
+        type=dir_path,
+        required=False,
+        default=".",
+        help="The location where the data should be saved",
+    )
+
+    args = parser.parse_args()
+
+    get_data(url, args.lang, args.save_dir)
